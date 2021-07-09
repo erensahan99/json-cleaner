b141e06dd76438c5d06f5155daff162de5c77c84
Eren <eren.sahan99@gmail.com>
2021-07-09 22:24:35 +0300

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,32 @@
+
+function clearOut() {
+    var words = $('#wordsTextArea').val().split('\n').filter(Boolean);
+    var jsonText = JSON.parse($('#jsonTextArea').val());
+    var removedKeys = []
+
+    Object.keys(jsonText).forEach(key => {
+        var searchFlag = false;
+        words.forEach(word => {
+                if (!word.localeCompare(key)) {
+                //console.log(word + " " + key + " => " + !word.localeCompare(key));
+                searchFlag = true;
+            }
+        });
+        if (!searchFlag) {
+            removedKeys.push(jsonText[key]);
+            delete jsonText[key];
+        }
+    });
+
+    console.log(jsonText);
+    console.log(Object.assign({}, removedKeys));
+
+    $('#resultTextArea').val(JSON.stringify(jsonText, undefined, 4))
+    $('#deletedTextArea').val(JSON.stringify(Object.assign({}, removedKeys), undefined, 4))
+    $('body,html').animate({scrollTop: 730}, 100); 
+}
+
+document.querySelector("#resultTextArea").onclick = function(){
+    document.querySelector("#resultTextArea").select();
+    document.execCommand('copy');
+}
