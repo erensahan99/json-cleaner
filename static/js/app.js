a1e3258782843dedb72599f9d57b8925a47303fa
Eren <eren.sahan99@gmail.com>
2021-07-09 22:50:31 +0300

    Deleted keys fixed

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 function clearOut() {
     var words = $('#wordsTextArea').val().split('\n').filter(Boolean);
     var jsonText = JSON.parse($('#jsonTextArea').val());
-    var removedKeys = []
+    var removedKeys = {}
 
     Object.keys(jsonText).forEach(key => {
         var searchFlag = false;
@@ -13,20 +13,25 @@ function clearOut() {
             }
         });
         if (!searchFlag) {
-            removedKeys.push(jsonText[key]);
+            removedKeys[key]=jsonText[key]
+            // removedKeys.push(key,JSON.stringify(jsonText[key]));
             delete jsonText[key];
         }
     });
 
     console.log(jsonText);
-    console.log(Object.assign({}, removedKeys));
+    console.log(removedKeys);
 
     $('#resultTextArea').val(JSON.stringify(jsonText, undefined, 4))
-    $('#deletedTextArea').val(JSON.stringify(Object.assign({}, removedKeys), undefined, 4))
+    $('#deletedTextArea').val(JSON.stringify(removedKeys, undefined, 4))
     $('body,html').animate({scrollTop: 730}, 100); 
 }
-
 document.querySelector("#resultTextArea").onclick = function(){
     document.querySelector("#resultTextArea").select();
+    document.execCommand('copy');
+}
+
+document.querySelector("#deletedTextArea").onclick = function(){
+    document.querySelector("#deletedTextArea").select();
     document.execCommand('copy');
 }
